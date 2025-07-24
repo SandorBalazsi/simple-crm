6611bdeb3f12fda21c2faf4e5c589a9e4a95ca64
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-07-24 16:32:28 +0200

    fixed material design false look

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -3,12 +3,14 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { MatInputModule } from "@angular/material/input";
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [MatDialogModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.css'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 
@@ -11,7 +11,7 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 
 @Component({
   selector: 'app-user',
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
